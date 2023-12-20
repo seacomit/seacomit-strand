ac3e6aeefcb72f589555027043d356a7f5f6fd03
Seacomit <86527888+seacomit@users.noreply.github.com>
2023-12-19 19:06:20 -0500

    added split arc rendering

--- src/drawing/StrandRenderer.ts
+++ src/drawing/StrandRenderer.ts
@@ -12,15 +12,15 @@ class StrandRenderer {
     numberCache: NumberProperties[] = [];
 
     render(ctx: CanvasRenderingContext2D, width: number, height: number, offset: number) {
-        ctx.fillStyle = '#140033';
+        ctx.fillStyle = '#050008';//'#140033';
         ctx.fillRect(0, 0, width, height);
 
         ctx.font = "10px Roboto";
 
-        const firstN = 41;
-        const quantity = 100;
+        const firstN = 17;
+        const quantity = 80;
         const highestVisibleN = quantity + offset;
-
+       
         // Update the cache if necessary.
         if (highestVisibleN > this.numberCache.length) {
           // calculate values up to highestVisibleN
@@ -43,32 +43,56 @@ class StrandRenderer {
         const gapBetweenN = sectionLength * 2/3;
 
         // Draw strand paths.
-        ctx.lineWidth = 4;
+        ctx.lineWidth = 8;
 
         let startsBelow = false;
         for (let p = 0; p < highestVisibleN; p++) {
           const triN = this.numberCache[p].n;
           let currentLoop = startsBelow;
+          const sqrInc = p * 2 + 1; // 1, 3, 5, 7, 9, 11, 13...
           for (let i = 0; i < 50; i++) {
-            const middlePoint = sectionLength * triN / 2;
             const multipleOffset = triN * sectionLength * i;
+            
+            const largeArcSize = triN - sqrInc;
+            
+            const laMiddlePoint = sectionLength * largeArcSize / 2;
             const centeredOffset = sectionLength / 2 + circleRadius / 2;
             const offsetOffset = sectionLength * offset * -1;
             const triangularOffset = sectionLength * p;
-            const finalOffset = middlePoint + multipleOffset + centeredOffset + offsetOffset + triangularOffset;
+            const finalOffset = laMiddlePoint + multipleOffset + centeredOffset + offsetOffset + triangularOffset;
   
-            const color = currentLoop ? "#00330088" : "#33003388";
-            const startAngle = currentLoop ? 0 : Math.PI;
-            const endAngle = currentLoop ? Math.PI : Math.PI * 2;
+            let color = startsBelow ? "#00330088" : "#33003388";
+            let startAngle = currentLoop ? 0 : Math.PI;
+            let endAngle = currentLoop ? Math.PI : Math.PI * 2;
             ctx.strokeStyle = color;
             ctx.beginPath();
             ctx.arc(
                 finalOffset, 
                 height / 2, 
-                sectionLength * triN / 2, 
+                sectionLength * largeArcSize / 2, 
                 startAngle, endAngle
             );
             ctx.stroke();
+            
+            color = currentLoop ? "#33000088" : "#00003388";
+            color = currentLoop ? "#ff338588" : "#ff338588";
+            currentLoop = !currentLoop;
+            startAngle = currentLoop ? 0 : Math.PI;
+            endAngle = currentLoop ? Math.PI : Math.PI * 2;
+
+            const smallArcSize = sqrInc;
+            const saMiddlePoint = sectionLength * smallArcSize / 2;
+
+            ctx.strokeStyle = color;
+            ctx.beginPath();
+            ctx.arc(
+                saMiddlePoint + multipleOffset + centeredOffset + offsetOffset + triangularOffset + sectionLength * largeArcSize, 
+                height / 2, 
+                sectionLength * smallArcSize / 2, 
+                startAngle, endAngle
+            );
+            ctx.stroke();
+
             currentLoop = !currentLoop;
           }
           startsBelow = !startsBelow;
