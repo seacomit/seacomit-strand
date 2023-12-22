889ef43144f1a590427e1d879f5448988cdcc53b
Seacomit <86527888+seacomit@users.noreply.github.com>
2023-12-21 22:41:15 -0500

    adding more tests

--- src/drawing/TriangularStrand.test.ts
+++ src/drawing/TriangularStrand.test.ts
@@ -91,4 +91,82 @@ test('TriangularStrand returns the correct number when triple compositing.', () 
 
   numberProps = tsc.get(6);
   expect(numberProps!.n).toEqual(3263459);
+
+
+
+  numberProps = tsc.get(7);
+  expect(numberProps!.n).toEqual(10192073);
+  numberProps = tsc.get(8);
+  expect(numberProps!.n).toEqual(27630809);
+  numberProps = tsc.get(9);
+  expect(numberProps!.n).toEqual(67084307);
+  numberProps = tsc.get(10);
+  expect(numberProps!.n).toEqual(149096327);
+  numberProps = tsc.get(11);
+  expect(numberProps!.n).toEqual(308230709);
+  numberProps = tsc.get(12);
+  expect(numberProps!.n).toEqual(599882573);
+  numberProps = tsc.get(13);
+  expect(numberProps!.n).toEqual(1109322959);
+  numberProps = tsc.get(14);
+  expect(numberProps!.n).toEqual(1963420427);
+  numberProps = tsc.get(15);
+  expect(numberProps!.n).toEqual(3345523457);
+  numberProps = tsc.get(16);
+  expect(numberProps!.n).toEqual(5514027809);
+  numberProps = tsc.get(17);
+  expect(numberProps!.n).toEqual(8825193323);
+  numberProps = tsc.get(18);
+  expect(numberProps!.n).toEqual(13760814959);
+  numberProps = tsc.get(19);
+  expect(numberProps!.n).toEqual(20961393197);
+  numberProps = tsc.get(20);
+  expect(numberProps!.n).toEqual(31265489237);
+  numberProps = tsc.get(21);
+  expect(numberProps!.n).toEqual(45755990759);
+  numberProps = tsc.get(22);
+  expect(numberProps!.n).toEqual(65814054323);
+  numberProps = tsc.get(23);
+  expect(numberProps!.n).toEqual(93181530809);
+  numberProps = tsc.get(24);
+  expect(numberProps!.n).toEqual(130032720617);
+
+  numberProps = tsc.get(25);
+  expect(numberProps!.n).toEqual(179056345667);
+
+  numberProps = tsc.get(26);
+  expect(numberProps!.n).toEqual(243548665559);
+  numberProps = tsc.get(27);
+  expect(numberProps!.n).toEqual(327518705573);
+  numberProps = tsc.get(28);
+  expect(numberProps!.n).toEqual(435806604509);
+
+
+  numberProps = tsc.get(29);
+  expect(numberProps!.n).toEqual(574216130687);
+  numberProps = tsc.get(30);
+  expect(numberProps!.n).toEqual(749662454747);
+  numberProps = tsc.get(31);
+  expect(numberProps!.n).toEqual(970336308209);
+  numberProps = tsc.get(32);
+  expect(numberProps!.n).toEqual(1245885697073);
+  numberProps = tsc.get(33);
+  expect(numberProps!.n).toEqual(1587616380059);
+
+
+  numberProps = tsc.get(34);
+  expect(numberProps!.n).toEqual(2008712361407);
+  numberProps = tsc.get(35);
+  expect(numberProps!.n).toEqual(2524477688477);
+  numberProps = tsc.get(36);
+  expect(numberProps!.n).toEqual(3152600884709);
+  numberProps = tsc.get(37);
+  expect(numberProps!.n).toEqual(3913443388823);
+  numberProps = tsc.get(38);
+  expect(numberProps!.n).toEqual(4830353411459);
+
+  // 98 in this case is the max before we exceed max safe int.
+  numberProps = tsc.get(98);
+  expect(numberProps!.n).toEqual(8862058406050559);
+  expect(numberProps!.n).toBeLessThan(Number.MAX_SAFE_INTEGER);
 });
